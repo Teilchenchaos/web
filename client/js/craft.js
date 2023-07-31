140f59101d86a5fb3e94bedbde9ef6fe8ae829fb
PoisChiche <mx.konovets@gmail.com>
2023-07-31 23:22:42 +0300

    so viele cgange ich bin zu etonner

--- client/js/craft.js
+++ client/js/craft.js
@@ -1,3 +1,4 @@
 function showCraftMenu() {
     document.getElementById('middlePart-iframe').innerHTML = '<iframe src="craft.html" id="middlePart-iframe-iframe" frameborder="0"></iframe>'
 }
+
---
+++ client/js/particlesMenu.js
@@ -0,0 +1,3 @@
+function showParticles() {
+  document.getElementById('middlePart-iframe').innerHTML = '<iframe src="ParticlesMenu.html" id="middlePart-iframe-iframe-iframe" frameborder="0"></iframe>'
+}
