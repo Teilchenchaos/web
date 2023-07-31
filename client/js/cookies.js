e14e7325a07d6b483d3afeec5fe38449557c6956
pfannkuchen <nathan.pioucraft@outlook.com>
2023-07-31 10:33:10 +0200

    add cookies button

--- client/js/cookies.js
+++ client/js/cookies.js
@@ -2,10 +2,14 @@ let cookiesConsent = getCookie("cookiesConsent")
 
 window.onload = function(){
     if(cookiesConsent == "") {
-        document.body.innerHTML = `<div id="cookies"><h1 id="cookiesTitle">Cookies</h1><p id="cookiesText">This website uses cookies to store save data and your account credentials, but cookies will never be used to track you.</p><p id="cookiesText">Ce site internet utilise les cookies pour stocker les données de sauvegarde et les informations de votre compte, mais ils ne seront jamais utilisés pour vous tracker.</p><p id="cookiesText">tiu retejo uzas biskvitojn, por konservi vian sekurkopion kaj vian konton, sed ni neniam uzos ilin por kunordigi vin</p><p id="cookiesText">Diese Website verwendet Cookies, um Daten und Ihre Kontodaten zu speichern, aber Cookies werden niemals verwendet, um Sie zu verfolgen.</p></div>`
+        document.body.innerHTML = `<div id="cookies"><h1 id="cookiesTitle">Cookies</h1><p id="cookiesText">This website uses cookies to store save data and your account credentials, but cookies will never be used to track you.</p><p id="cookiesText">Ce site internet utilise les cookies pour stocker les données de sauvegarde et les informations de votre compte, mais ils ne seront jamais utilisés pour vous tracker.</p><p id="cookiesText">tiu retejo uzas biskvitojn, por konservi vian sekurkopion kaj vian konton, sed ni neniam uzos ilin por kunordigi vin.</p><p id="cookiesText">Diese Website verwendet Cookies, um Daten und Ihre Kontodaten zu speichern, aber Cookies werden niemals verwendet, um Sie zu verfolgen.</p><button id="cookies-button" onclick="acceptCookies()">OK</button></div>`
     } 
 }
 
+function acceptCookies() {
+    setCookie("cookiesConsent", "true", 100)
+    location.reload()
+}
 
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
