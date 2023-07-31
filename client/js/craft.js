056865cf409768d6dc2f1dab68b32798ae16d818
pfannkuchen <nathan.pioucraft@outlook.com>
2023-07-31 18:38:50 +0200

    fix bugs and add a bit of css

--- client/js/craft.js
+++ client/js/craft.js
@@ -1,3 +1,3 @@
 function showCraftMenu() {
-    document.getElementById('middlePart-iframe').innerHTML = '<iframe src="craft.html" frameborder="0"></iframe>'
+    document.getElementById('middlePart-iframe').innerHTML = '<iframe src="craft.html" id="middlePart-iframe-iframe" frameborder="0"></iframe>'
 }
