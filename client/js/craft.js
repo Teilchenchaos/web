8f936a4d33cab7df632718e3c8e30ef13203673c
pfannkuchen <nathan.pioucraft@outlook.com>
2023-08-01 10:39:28 +0200

    you can add quarks to the crafting table (just visual)

--- client/js/craft.js
+++ client/js/craft.js
@@ -3,6 +3,11 @@ function insertParticle(position) {
         document.getElementById(`craftTable-${position}`).innerHTML = ""
     }
     else {
-        document.getElementById("craftTable").innerHTML += "<div class='dialog'><h3>Please, choose to put an up or a down quark.</h3><button class='dialog-button'>Up</button class='dialog-button'><button>Down</button></div>"
+        document.getElementById("craftTable").innerHTML += `<div class='dialog' id="dialog"><h3>What type of quark do you want to put in this slot ?.</h3><button class='dialog-button' onclick='putQuarkInCraftSlot("up", ${position})'>Up</button><button class='dialog-button' onclick='putQuarkInCraftSlot("down", ${position})'>Down</button></div>`
     }
+}
+
+function putQuarkInCraftSlot(quark, position) {
+    document.getElementById(`craftTable-${position}`).innerHTML = quark
+    document.getElementById("dialog").remove()
 }
