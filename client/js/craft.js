0b06cfd0559473ffbf96c52903922cbb766fe582
pfannkuchen <nathan.pioucraft@outlook.com>
2023-08-01 10:30:17 +0200

    advance in the crafting system

--- client/js/craft.js
+++ client/js/craft.js
@@ -1,3 +1,8 @@
 function insertParticle(position) {
-    console.log(position)
+    if(document.getElementById(`craftTable-${position}`).innerHTML != "") {
+        document.getElementById(`craftTable-${position}`).innerHTML = ""
+    }
+    else {
+        document.getElementById("craftTable").innerHTML += "<div class='dialog'><h3>Please, choose to put an up or a down quark.</h3><button class='dialog-button'>Up</button class='dialog-button'><button>Down</button></div>"
+    }
 }
