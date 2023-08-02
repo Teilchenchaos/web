571a976a0ec4cdc7afb490b7f24bd0cd7d90cb9a
pfannkuchen <nathan.pioucraft@outlook.com>
2023-08-02 21:04:02 +0200

    I will finish tommmorow

--- client/js/periodicTable.js
+++ client/js/periodicTable.js
@@ -4,6 +4,8 @@ function loadPeriodicTable() {
         for(let i=0; i<data["elements"].length; i++) {
             console.log(data["elements"][i]["number"])
             document.getElementById(`periodicTable-element-${data["elements"][i]["number"]}`).innerHTML = `<p style="font-size: 0.5vw; margin-left: -2vw; margin-top: -0.1vw">${data["elements"][i]["number"]}</p><h6 style="margin-top: 0.5vw">${data["elements"][i]["symbol"]}</h6><p style="font-size: 0.5vw; margin-top: -1vw">${data["elements"][i]["name"]}</p>`
+            document.getElementById(`periodicTable-element-${data["elements"][i]["number"]}`).onclick = function () { window.open("atom.html?m=" + i) }
+
         }
     })
 }
